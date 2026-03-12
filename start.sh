5cdf259b54ff2b2bdad751e2882d7a139d3bf734
KishoreKumarDC <kishorekumardc2004@gmail.com>
2026-03-12 13:26:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
-uvicorn app:app --host 0.0.0.0 --port $PORT
+#!/usr/bin/env bash
+
+echo "Starting FastAPI..."
+uvicorn backend.app:app --host 0.0.0.0 --port $PORT &
+
+echo "Starting Telegram bot..."
+python telegram_bot.py
